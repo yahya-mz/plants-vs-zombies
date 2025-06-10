e314ae3427bea65d38cb36a58989b0ea42c00b3a
Amin.Karvizi <Amin.Karvizi@payever.org>
2025-06-10 14:28:01 +0330

    Initial Commit

---
+++ src/main/java/com/pvz/plantsvszombies/HelloController.java
@@ -0,0 +1,14 @@
+package com.pvz.plantsvszombies;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
